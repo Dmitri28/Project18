b6e38627445b03fb0dbd60ec1affe67716081a43
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-10-31 13:10:39 +0200

    Project18
    Project18

---
+++ src/space/Space.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package space;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class Space {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+      String k=in.nextLine();
+      
+      
+      System.out.println(k+k);
+       
+    }
+    
+}
